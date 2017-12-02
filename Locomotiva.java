ce7fb1c25c3762b73a8d5e21de9dee8c1a56fada
gihcapel <gihccapel@gmail.com>
2017-12-02 14:07:35 -0200

    Add files via upload

---
+++ Locomotiva.java
@@ -0,0 +1,45 @@
+package ComposicaoFerroviaria;
+
+public class Locomotiva {
+	
+	private int classe;
+	private String descricao;
+	private double pesoMaxRebocavel;
+	private double bitola;
+	private double comprimentoLocomotiva;
+	
+
+	public int getClasse() {
+		return classe;
+	}
+	public void setClasse(int classe) {
+		this.classe = classe;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getPesoMaxRebocavel() {
+		return pesoMaxRebocavel;
+	}
+	public void setPesoMaxRebocavel(double pesoMaxRebocavel) {
+		this.pesoMaxRebocavel = pesoMaxRebocavel;
+	}
+	public double getBitola() {
+		return bitola;
+	}
+	public void setBitola(double bitola) {
+		this.bitola = bitola;
+	}
+	public double getComprimentoLocomotiva() {
+		return comprimentoLocomotiva;
+	}
+	public void setComprimentoLocomotiva(double comprimentoLocomotiva) {
+		this.comprimentoLocomotiva = comprimentoLocomotiva;
+	}
+	
+	
+
+}
---
+++ Vagao.java
@@ -0,0 +1,47 @@
+package ComposicaoFerroviaria;
+
+public class Vagao {
+	
+	private String tipo;
+	private String subtipo;
+	private String pesoMaxEBitola;
+	private int proprietario;
+	private double comprimentoVagao;
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getSubtipo() {
+		return subtipo;
+	}
+	public void setSubtipo(String subtipo) {
+		this.subtipo = subtipo;
+	}
+	public String getPesoMaxEBitola() {
+		return pesoMaxEBitola;
+	}
+	public void setPesoMaxEBitola(String pesoMaxEBitola) {
+		this.pesoMaxEBitola = pesoMaxEBitola;
+	}
+	public int getProprietario() {
+		return proprietario;
+	}
+	public void setProprietario(int proprietario) {
+		this.proprietario = proprietario;
+	}
+	public double getComprimentoVagao() {
+		return comprimentoVagao;
+	}
+	public void setComprimentoVagao(double comprimentoVagao) {
+		this.comprimentoVagao = comprimentoVagao;
+	}
+	
+	private void adicionarVagao(){
+		
+	}
+	
+
+}
