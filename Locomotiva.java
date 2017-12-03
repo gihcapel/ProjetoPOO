1e1d0123d0114846710afb965fec0fbb1d49e453
gihcapel <gihccapel@gmail.com>
2017-12-03 17:35:36 -0200

    Update Locomotiva.java

--- Locomotiva.java
+++ Locomotiva.java
@@ -1,53 +1,69 @@
 
-public class Locomotiva {
-	private int classe;
-	private String descricao;
-	private double pesoMaxLoc, bitolaLoc, comprimentoLoc;
-	
-	public int getClasse() {
-		return classe;
-	}
-	public void setClasse(int classe) {
-		this.classe = classe;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	public double getPesoMaxLoc() {
-		return pesoMaxLoc;
-	}
-	public void setPesoMaxLoc(double pesoMaxLoc) {
-		this.pesoMaxLoc = pesoMaxLoc;
-	}
-	public double getBitolaLoc() {
-		return bitolaLoc;
-	}
-	public void setBitolaLoc(double bitolaLoc) {
-		this.bitolaLoc = bitolaLoc;
-	}
-	public double getComprimentoLoc() {
-		return comprimentoLoc;
-	}
-	public void setComprimentoLoc(double comprimentoLoc) {
-		this.comprimentoLoc = comprimentoLoc;
-	}
-	
-	public Locomotiva(int classe, String descricao, double pesoMaxLoc, double bitolaLoc, double comprimentoLoc) {
-		this.classe = classe;
-		this.descricao = descricao;
-		this.pesoMaxLoc = pesoMaxLoc;
-		this.bitolaLoc = bitolaLoc;
-		this.comprimentoLoc = comprimentoLoc;
-	}
-	@Override
-	public String toString() {
-		return "Locomotiva [classe=" + classe + ", descricao=" + descricao + ", pesoMaxLoc=" + pesoMaxLoc
-				+ ", bitolaLoc=" + bitolaLoc + ", comprimentoLoc=" + comprimentoLoc + "]";
-	}
+package ComposicaoFerroviaria;
+  
+  public class Locomotiva {
+ -	
+ -	//Componentes da classe locomotiva
+  	private int classe;
+  	private String descricao;
+ -	private double pesoMaxRebocavel;
+ -	private double bitola;
+ -	private double comprimentoLocomotiva;
+ +	private double pesoMaxLoc, bitolaLoc, comprimentoLoc;
+  	
+ -	//Métodos getters e setters da classe locomotiva
+  	public int getClasse() {
+  		return classe;
+  	}
+ @@ -22,25 +16,38 @@ public String getDescricao() {
+  	public void setDescricao(String descricao) {
+  		this.descricao = descricao;
+  	}
+ -	public double getPesoMaxRebocavel() {
+ -		return pesoMaxRebocavel;
+ +	public double getPesoMaxLoc() {
+ +		return pesoMaxLoc;
+  	}
+ -	public void setPesoMaxRebocavel(double pesoMaxRebocavel) {
+ -		this.pesoMaxRebocavel = pesoMaxRebocavel;
+ +	public void setPesoMaxLoc(double pesoMaxLoc) {
+ +		this.pesoMaxLoc = pesoMaxLoc;
+  	}
+ -	public double getBitola() {
+ -		return bitola;
+ +	public double getBitolaLoc() {
+ +		return bitolaLoc;
+  	}
+ -	public void setBitola(double bitola) {
+ -		this.bitola = bitola;
+ +	public void setBitolaLoc(double bitolaLoc) {
+ +		this.bitolaLoc = bitolaLoc;
+  	}
+ -	public double getComprimentoLocomotiva() {
+ -		return comprimentoLocomotiva;
+ +	public double getComprimentoLoc() {
+ +		return comprimentoLoc;
+  	}
+ -	public void setComprimentoLocomotiva(double comprimentoLocomotiva) {
+ -		this.comprimentoLocomotiva = comprimentoLocomotiva;
+ +	public void setComprimentoLoc(double comprimentoLoc) {
+ +		this.comprimentoLoc = comprimentoLoc;
+  	}
+  	
+ +	public Locomotiva(int classe, String descricao, double pesoMaxLoc, double bitolaLoc, double comprimentoLoc) {
+ +		this.classe = classe;
+ +		this.descricao = descricao;
+ +		this.pesoMaxLoc = pesoMaxLoc;
+ +		this.bitolaLoc = bitolaLoc;
+ +		this.comprimentoLoc = comprimentoLoc;
+ +	}
+ +	@Override
+ +	public String toString() {
+ +		return "Locomotiva [classe=" + classe + ", descricao=" + descricao + ", pesoMaxLoc=" + pesoMaxLoc
+ +				+ ", bitolaLoc=" + bitolaLoc + ", comprimentoLoc=" + comprimentoLoc + "]";
+ +	}
+ +
+  	
+  
+  }
 
-	
-
-}
