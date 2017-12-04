c33bf5a69a8320e60d47d9071cc1baf4b86fd2e0
gihcapel <gihccapel@gmail.com>
2017-12-04 11:40:12 -0200

    Update JanelaMenu.java

--- JanelaMenu.java
+++ JanelaMenu.java
@@ -1,21 +1,26 @@
 package interfacagrafica;
 
+import java.awt.*;
 import java.awt.GridLayout;
 
 import javax.swing.*;
 
-public class JanelaMenu extends JFrame{
-	private JPanel painel= new JPanel();
-	private JButton btnCadastrarLocomotiva, btnCadastrarVagao,btnComposicao, btnListaLocomotiva,btnListaVagao;
+public class JanelaMenu extends JFrame{ 
+    
+	JPanel painel= new JPanel();
+	private JButton btnCadastrarLocomotiva, btnCadastrarVagao, btnComposicao, btnListaLocomotiva, btnListaVagao;
+        
 	JanelaMenu(){
-		super("ComposiÁ„o Ferrovi·ria");
+		super("Composi√ß√£o Ferrovi√°ria");
 		btnCadastrarLocomotiva = new JButton("Cadastrar Locomotiva");
-		btnCadastrarVagao = new JButton("Cadastrar Vag„o");
-		btnComposicao = new JButton("ComposiÁ„o");
+		btnCadastrarVagao = new JButton("Cadastrar Vag√£o");
+		btnComposicao = new JButton("Composi√ß√£o");
 		btnListaLocomotiva = new JButton("Consultar/Remover Locomotiva");
-		btnListaVagao = new JButton("Consultar/Remover Vag„o");
+		btnListaVagao = new JButton("Consultar/Remover Vag√£o");
 		
 		painel.setLayout(new GridLayout(7, 2, 5, 5));
+                painel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
+                painel.setBackground(Color.WHITE);
 		
 		painel.add(btnCadastrarLocomotiva);
 		painel.add(btnListaLocomotiva);
@@ -34,6 +39,7 @@ public class JanelaMenu extends JFrame{
             	JanelaLocomotiva l = new JanelaLocomotiva();
         		l.setVisible(true);
         		l.setSize(680,480);
+                        l.setLocation(300,300);
             }                            
         });
 		btnListaLocomotiva.addActionListener(new java.awt.event.ActionListener(){
@@ -45,6 +51,10 @@ public class JanelaMenu extends JFrame{
 		btnCadastrarVagao.addActionListener(new java.awt.event.ActionListener(){
             @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JanelaVagao v = new JanelaVagao();
+                        v.setVisible(true);
+                        v.setSize(680,480);
+                        v.setLocation(300,300);
             	
             }                            
         });
