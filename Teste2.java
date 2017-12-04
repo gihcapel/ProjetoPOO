192d5faa2eb3b130e2446d63b499dae672b29eb9
gihcapel <gihccapel@gmail.com>
2017-12-04 14:46:34 -0200

    Add files via upload

---
+++ Teste2.java
@@ -0,0 +1,31 @@
+
+package poo;
+
+public class Teste2 {
+    
+    public static void main(String[] args) {
+        /*JanelaVagao janelavagao = new JanelaVagao();
+        janelavagao.setVisible(true);
+        janelavagao.setSize(500,500);
+        janelavagao.setLocation(300,300);
+        janelavagao.show();
+        
+        JanelaLocomotiva janela = new JanelaLocomotiva();
+        janela.setVisible(true);
+        janela.setSize(500,500);
+        janela.setLocation(300,300);
+        janela.show();
+        
+        JanelaMenu janelamenu = new JanelaMenu();
+        janelamenu.show();
+        janelamenu.setSize(500,500);
+        janelamenu.setLocation(300,300);*/
+        
+        JanelaComposicao janelacomp = new JanelaComposicao();
+        janelacomp.show();
+        janelacomp.setSize(500,500);
+        janelacomp.setLocation(300,300);
+        
+    }
+    
+}
